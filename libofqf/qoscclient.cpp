4a2d1222d7b4e3101f5fa2c66512632b9c45c8df
Arnold Krille <arnold@arnoldarts.de>
2007-04-14 21:26:16 +0000

    ignore me

--- libofqf/qoscclient.cpp
+++ libofqf/qoscclient.cpp
@@ -27,9 +27,6 @@ void QOscClient::setAddress( const QHostAddress& address, quint16 port ) {
 
 void QOscClient::sendData( QString path, QVariant data ) {
 	qDebug() << "QOscClient::sendData(" << path << "," << data << ")";
-	/*QByteArray tmp = oscMessage( path, data );
-	qDebug() << " length:" << tmp.length();
-	d->socket->writeDatagram( tmp, d->address, d->port );*/
 	lo_message msg = lo_message_new();
 	if ( data.type() == QVariant::Int )
 		lo_message_add_int32( msg, data.toInt() );
@@ -42,13 +39,13 @@ void QOscClient::sendData( QString path, QVariant data ) {
 	qDebug() << " osc-message at" << chars << " length is" << charsize;
 	QByteArray out = oscMessage( path, data );
 	qDebug() << " own message has size" << out.size();
-	for ( int i=0; i<qMax( int( charsize ), out.size() ); ++i ) {
-		qDebug() << "  byte" << i << "\t" << static_cast<quint8*>( chars )[ i ] /*<< "\"" << static_cast<char*>( chars )[ i ] << "\""*/ << "\t :" << quint8( out[ i ] ) << "\t\"" << out[ i ] << "\"";
-	}
-	//QByteArray out( static_cast<char*>( chars ), charsize );
+	//for ( int i=0; i<qMax( int( charsize ), out.size() ); ++i )
+	//	qDebug() << "  byte" << i << "\t" << static_cast<quint8*>( chars )[ i ] /*<< "\"" << static_cast<char*>( chars )[ i ] << "\""*/ << "\t :" << quint8( out[ i ] ) << "\t\"" << out[ i ] << "\"";
 	socket->writeDatagram( out, _address, _port );
 }
 void QOscClient::sendData( QString path, QList<QVariant> data ) {
 	qDebug() << "QOscClient::sendData(" << path << "," << data << ")";
+	QByteArray out = oscMessage( path, data );
+	socket->writeDatagram( out, _address, _port );
 }
 
